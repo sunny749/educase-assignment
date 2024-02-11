7ba971818ffb02dc9ba1a75dd87f9da68ba5732d
sunny749 <sunnygante12345@gmail.com>
2024-02-11 13:04:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './App.css';
-import GridGame from './components/GridGame'
-import {AnimalGame} from './components/AnimalGame'
-import CircleGame from './components/CircleGame';
-import Dark_mode from './components/dark_mode/Dark_mode';
 import Main from './components/educaseAssignment/Main';
 
 function App() {
